f8a674c1c8ea7c6a1ff6ea9e13f49bfcdb89ca6f
yuliasalsa <yuliasalsa04@gmail.com>
2021-05-10 19:41:50 +0700

    [caca]-add activity_goals_reached.xml

---
+++ app/src/main/java/com/example/stepcounter/activity_goals_reached.kt
@@ -0,0 +1,11 @@
+package com.example.stepcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_goals_reached : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goals_reached)
+    }
+}
