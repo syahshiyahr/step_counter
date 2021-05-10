d28b024a31aab03c1c89d27960bff9c2db7887a7
atikahnabila <atikahnabila21@gmail.com>
2021-05-10 20:59:16 +0700

    [Atikah]-add fragment_add.xml

--- app/src/main/java/com/example/stepcounter/GoalsReachedActivity.kt
+++ app/src/main/java/com/example/stepcounter/GoalsReachedActivity.kt
@@ -3,7 +3,7 @@ package com.example.stepcounter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_goals_reached : AppCompatActivity() {
+class GoalsReachedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goals_reached)
